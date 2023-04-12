d6d5d505ab7d20731c33b787150e13acef4bc97f
Manish Sharma <ms8750122327@gmail.com>
2023-04-12 22:39:24 +0530

    Simple Product Feature Added - Working / Uploading image to S3

---
+++ ecom/products/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+# Create your models here.
+class Category(models.Model):
+    title = models.CharField(max_length=20)
+    
+    def __str__(self):
+        return self.title
+    
+class Product(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6,decimal_places=2)
+    image = models.ImageField(upload_to="media/")
+    category = models.ForeignKey(Category,on_delete=models.SET_NULL,null=True)
+    stock = models.PositiveBigIntegerField(default=0)
+    
+    def __str__(self):
+        return self.title
